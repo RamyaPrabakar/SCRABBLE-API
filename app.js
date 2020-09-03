4659df9abffc15584fdc9aab85c7fa8cf53dd499
RamyaPrabakar <ramyaprabakar01@gmail.com>
2020-09-03 16:48:46 +0530

    established database connection, code for permutation done

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const mysql = require('mysql');
 
 const app = express();
+//const jsonParser = bodyParser.json();
+
+//let connected = false;
+
+const mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'tribias4',
+  database: 'scrabbleDB',
+  multipleStatements: true,
+});
+
+mysqlConnection.connect((err) => {
+  if (err) {
+    console.log('Connection Failed');
+  } else {
+    console.log('Connected');
+    connected = true;
+  }
+});
 
 
 app.set("view engine", "ejs");
@@ -16,8 +37,16 @@ app.get("/", (req, res) => { res.render("index") });
 });*/
 
 app.get('/scrabble/:data', (req, res) => {
-    res.render("index1", { letters:req.params.data });
+    res.render("index1", { letters: req.params.data });
     console.log(req.params.data);
 })
+
+/*app.post('/checkDatabase', jsonParser, (req, res) => {
+  if (connected) {
+    const sql1 = `INSERT INTO todolist (task_name) VALUES ("${req.body.taskName}")`;
+    mysqlConnection.query(sql1, (err) => {
+      if (err) throw err;
+    });
+});*/
 
 app.listen(3000);
